e20fbbd9e0726f9636713c04edc5cff59f9ae952
Umasankar Srinivas Varma Pusapati <upusapat@uncc.edu>
2018-05-31 15:03:19 -0400

    Login activity test cases using Mockito

--- app/src/test/java/org/example/gayathri/myfirstapp/login/LoginPresenterTest.java
+++ app/src/test/java/org/example/gayathri/myfirstapp/login/LoginPresenterTest.java
@@ -1,38 +1,67 @@
 package org.example.gayathri.myfirstapp.login;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
-
+@RunWith(MockitoJUnitRunner.class)
 public class LoginPresenterTest {
 
-    private LoginPresenter mLoginPresentor;
-    private LoginContract.View mView;
-    private LoginContract.Interactor mInteractor;
+    private LoginPresenter presentor;
+    @Mock
+    private LoginContract.View view;
+    @Mock
+    private LoginContract.Interactor interactor;
 
     @Before
     public void setUp() throws Exception {
-        mView = Mockito.mock(LoginContract.View.class);
-        mInteractor = Mockito.mock(LoginContract.Interactor.class);
-        mLoginPresentor = new LoginPresenter(this.mView, mInteractor);
+        presentor = new LoginPresenter(view, interactor);
     }
 
     @Test
-    public void validateAndLogin() {
+    public void validateAndLogin() throws Exception {
+        // Given
+
+        // When
+        presentor.validateAndLogin("username", "password");
+
+        // Then
+        Mockito.verify(interactor).login("username", "password", presentor);
     }
 
     @Test
-    public void onUsernameError() {
+    public void onUsernameError() throws Exception {
+        // Given
+
+        // When
+        presentor.onUsernameError();
+
+        // Then
+        Mockito.verify(view).setUsernameError();
     }
 
     @Test
-    public void onPasswordError() {
+    public void onPasswordError() throws Exception {
+        // Given
+
+        // When
+        presentor.onPasswordError();
+
+        // Then
+        Mockito.verify(view).setPasswordError();
     }
 
     @Test
-    public void onSuccess() {
+    public void onSuccess() throws Exception {
+        // Given
+
+        // When
+        presentor.onSuccess();
+
+        // Then
+        Mockito.verify(view).goToMainActivity();
     }
 }
